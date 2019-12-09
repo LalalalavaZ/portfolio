3ddc5826cd3b569ad976a1adeaff86d1bcc5ed32
LalalalavaZ <43247224+LalalalavaZ@users.noreply.github.com>
2019-12-08 21:04:55 -0500

    2019128

---
+++ main.js
@@ -0,0 +1,12 @@
+// menu scroll disappear
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("menu").style.top = "0";
+      } else {
+        document.getElementById("menu").style.top = "-300px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
