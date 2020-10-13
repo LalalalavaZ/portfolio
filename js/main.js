d109e44e6c725f77708aa99fe0210b1d7d3e4c20
lavaz <lava@lavazs-MacBook-Air.local>
2020-10-13 07:10:08 -0400

    20201013

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// menu scroll disappear
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("menu").style.top = "0";
+      } else {
+        document.getElementById("menu").style.top = "-300px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+
+
+
+
+
+//blk button next
+
+// var buttnext = document.getElementById("buttnext");
+// buttnext.onclick = function(){
+//   document.getElementById("blkcnt");
+// }
