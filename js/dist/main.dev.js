d76e4928eb5d24c139958557b298d4ec0651773f
lavaz <lava@lavazs-MacBook-Air.local>
2020-10-15 15:16:52 -0400

    20201015

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,5 +1,4 @@
 // menu scroll disappear
-
 // var prevScrollpos = window.pageYOffset;
 // window.onscroll = function() {
 //     var currentScrollPos = window.pageYOffset;
@@ -10,16 +9,12 @@
 //       }
 //       prevScrollpos = currentScrollPos;
 //     }
-
-
 //hide mainsec by menu
-
-
 //cursor prtfsec
 // mouseX = event => {
 //     return event.clientX;
 // };
-
 // mouseY = event => {
 //     return event.clientY;
 // };
+"use strict";
