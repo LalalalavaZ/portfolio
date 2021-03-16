fadf3dc304748c7f8f7cc5f56ecaed9741c87c8b
lavaz <lava@lavazs-MacBook-Air.local>
2021-03-16 08:43:26 -0400

    20210316

---
+++ 2020p/js/dist/main.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// menu scroll disappear
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//       if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("menu").style.top = "0";
+//       } else {
+//         document.getElementById("menu").style.top = "-300px";
+//       }
+//       prevScrollpos = currentScrollPos;
+//     }
+//hide mainsec by menu
+var menuSec = document.querySelector('.menusec'); // for (var i =0; i < menuSec.length; i++){
+//     menuSec[i].classList.add('hidden');   
+// }
+// console.log(menuSec.classList.contains("menusec"))
+// menuSec[0].classList.remove('hidden');
+
+var portfolioSec = document.querySelector("#portfolio");
+var randSec = document.querySelector("#randart");
+var abtmeSec = document.querySelector("#abtme"); // portfolioSec.classList.add('hidden');
+//cursor prtfsec
+// mouseX = event => {
+//     return event.clientX;
+// };
+// mouseY = event => {
+//     return event.clientY;
+// };
